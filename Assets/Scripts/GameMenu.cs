478a5cdf8f3e7d1b4666a8d5b58342c9a1a8c92b
TUFFY <emberhawk97@gmail.com>
2021-05-18 18:21:20 +0100

    Added player stat preference to gamemenu

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
+
+    private CharStats[] playerStats;
+
+    [SerializeField] Text[] nameText, hpText, mpText, lvlText, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] chatStatHolder;
+    
 
     private void Start() 
     {
@@ -24,10 +33,32 @@ public class GameMenu : MonoBehaviour
             else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
             }
         }
-        
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; i < playerStats.Length; i++)
+        {
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                chatStatHolder[i].SetActive(true);
+
+            }
+            else
+            {
+                chatStatHolder[i].SetActive(false);
+            }
+        }
+
+
+
+
     }
 
 }
