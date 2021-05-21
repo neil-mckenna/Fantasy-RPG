c11c8732655fb5cbc48c560c686505480790ea2b
TUFFY <emberhawk97@gmail.com>
2021-05-21 15:47:48 +0100

    Added battlechar script and created battle enemy prefabs

---
+++ Assets/Scripts/BattleChar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleChar : MonoBehaviour
+{
+    public bool isPlayer;
+    public string[] movesAvaiable;
+
+    public string charName;
+    public int currentHp, maxHP, currentMP, maximumMP, strength, defense, wpnPower, armorPower;
+    public bool hadDied;
+     
+
+
+
+
+}
