3df7f60a8ebadf28aa44c1cfaa98d0509fd709df
TUFFY <emberhawk97@gmail.com>
2021-05-21 17:19:02 +0100

    Added player and enemy battle prefabs are now loading into the battle scene

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -14,6 +14,7 @@ public class BattleManager : MonoBehaviour
     public BattleChar[] playersPrefabs;
     public BattleChar[] enemyPrefabs;
 
+    public List<BattleChar> activeBattlers = new List<BattleChar>();
 
 
     // static
@@ -31,7 +32,7 @@ public class BattleManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.T))
         {
-            BattleStart(new string[] { "Eyeball", "Spider" });
+            BattleStart(new string[] { "Eyeball", "Spider", "Skeleton" });
         }
 
 
@@ -45,10 +46,69 @@ public class BattleManager : MonoBehaviour
 
             GameManager.instance.battleActive = true;
 
-            battleScene.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
+            battleScene.transform.position = new Vector3(
+                Camera.main.transform.position.x, Camera.main.transform.position.y, transform.position.z);
             battleScene.SetActive(true);
 
             AudioManager.instance.PlayBGM(0);
+
+            // Instantiate player battle objects
+            for(int i = 0; i < playerPositions.Length; i++)
+            {
+                if(GameManager.instance.playerStats[i].gameObject.activeInHierarchy)
+                {
+                    for(int j = 0; j < playersPrefabs.Length; j++)
+                    {
+                        if(playersPrefabs[j].charName == GameManager.instance.playerStats[i].charName)
+                        {
+                            BattleChar newPlayer = Instantiate(playersPrefabs[j],playerPositions[i].position, playerPositions[i].rotation);
+
+                            newPlayer.transform.parent = playerPositions[i];
+
+                            activeBattlers.Add(newPlayer);
+
+                            CharStats thePlayer = GameManager.instance.playerStats[i];
+                            activeBattlers[i].currentHp = thePlayer.currentHP;
+                            activeBattlers[i].maxHP = thePlayer.maxHP;
+                            activeBattlers[i].currentMP = thePlayer.currentMP;
+                            activeBattlers[i].maximumMP = thePlayer.maximumMP;
+                            activeBattlers[i].strength = thePlayer.strength;
+                            activeBattlers[i].defense = thePlayer.defense;
+                            activeBattlers[i].wpnPower = thePlayer.wpnPower;
+                            activeBattlers[i].armorPower = thePlayer.armorPower;
+
+                        }
+
+                    }
+
+
+                }
+            }
+
+            for(int i = 0; i < enemiesToSpawn.Length; i++)
+            {
+                if(enemiesToSpawn[i] != "")
+                {
+                    for(int j = 0; j < enemyPrefabs.Length; j++)
+                    {
+                        if(enemyPrefabs[j].charName == enemiesToSpawn[i])
+                        {
+                            BattleChar newEnemy = Instantiate(enemyPrefabs[j], enemyPositions[i].position, enemyPositions[i].rotation);
+
+                            newEnemy.transform.parent = enemyPositions[i];
+
+                            activeBattlers.Add(newEnemy);
+
+
+
+                        }
+
+
+                    }
+                }
+
+
+            }
 
         }
 
